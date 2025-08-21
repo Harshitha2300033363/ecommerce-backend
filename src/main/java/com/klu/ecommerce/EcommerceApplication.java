325a84e86868473e6afa2aa5b2e31d1cbf0daf9e
Harshitha2300033363 <2300033363@kluniversity.in>
2025-08-21 12:47:25 +0530

    Updated main class for WAR deployment

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -8,12 +8,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class EcommerceApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
-    }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(EcommerceApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
     }
 }
